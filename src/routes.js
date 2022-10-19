48e9a40455ba6718cca3308840b4686cc1583c2e
Julien Verley <mjules@gmail.com>
2022-10-19 13:09:24 +0200

    Tests github pages deployment

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 // export const home = "/";
 // export const about = "/about";
 // export const location = "/location/:id";
-export const home = "/";
-export const about = "/about";
+export const home = "process.env.REACT_APP_FOR_PATH";
+export const about = "process.env.REACT_APP_FOR_PATH/about";
 export const location = "/location/:id";
